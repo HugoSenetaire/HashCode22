8cc558a9f41150a7cf29fb51567504c9ff0ca303
HugoSenetaire <hhjs@dtu.dk>
2022-02-24 20:46:22 +0100

    no idea

--- roll.py
+++ roll.py
@@ -1,29 +1,30 @@
 from update_fonction import update_t, update_choose
-
+from write_trajectory import write_trajectory
 
 def roll_project_list_project_possible(contributors, projects, cost_function, max_iter = 1e5):
   """
   Roll the project list until we fill all projects  
   #Parameters :
-  #   contributors : DF of contributors
+  #   contributors : list of <Contributor>
   #   projects : list of <Project>
+  #   cost_function : function cost that take as input a project, returns a float
 
   #Return :
   #   trajectory
 
   """
 
-  trajectory = str("")
+  trajectory = str("\n")
   t = 0
   while (not len(projects) > 0) and t<max_iter:
       t +=1
       update_t(projects, contributors, t)
       dispo_projects = project_possible_list(projects, contributors)
       while len(dispo_projects)>0 :
-          best_project = get_best_project(dispo_projects, cost_function)
-          best_contributor = get_best_contributor(best_project, contributors)          
-          update_choose(projects, contributors, best_project, best_contributor)
-          trajectory = write_trajectory(trajectory, best_project, best_contributor)
+          best_project = get_best_project(dispo_projects, cost_function) # Return a class Project element
+          best_contributors = get_best_contributor(best_project, contributors)       # Need a list of contributors, with role 1 in position 1 and so one   
+          update_choose(projects, contributors, best_project, best_contributors)
+          trajectory = write_trajectory(trajectory, best_project, best_contributors)
 
   return trajectory
         
@@ -34,16 +35,16 @@ def roll_project_list_project_possible(contributors, projects, cost_function, ma
   """
   Roll the project list until we fill all projects. Difference with previous is we treat contributor and project at the same time for value
   #Parameters :
-  #   contributors : DF of contributors
+  #   contributors : list of <Contributor>
   #   projects : list of <Project>
   #   cost_function : function cost that take as input a contributor and a project, returns a float
 
   #Return :
-  #   trajectory
+  #   trajectory : Txt file with the trajectory 
 
   """
 
-  trajectory = str("")
+  trajectory = str("\n")
   t = 0
   while (not len(projects) > 0) and t<max_iter:
       t +=1
