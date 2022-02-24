9196a8f76e0c45585f62c7ad30387fe875565b4a
JustineDorsz <justine.dorsz@eleves.enpc.fr>
2022-02-24 22:09:47 +0100

    Add cost function with coef

--- cost_funtion.py
+++ cost_funtion.py
@@ -1,2 +1,18 @@
+from parse_data import compute_skills, parse_data
+from paths import PATHS
+
 def cost_function(project, persons = None):
     return project.score_per_day
+
+
+def cost_function_with_skills(coef_score, coef_cost, project, persons, projects):
+    project_score = project.score_per_day
+    skills_demand_offer = compute_skills(persons, projects)
+    skill_cost = 0
+    for role in project.roles:
+        level = role.skill_level
+        skill_cost += (
+            skills_demand_offer[role.skill_name][f"nb_projects_{level}"]
+            / skills_demand_offer[role.skill_name][f"nb_contribs_{level}"]
+        )
+    return coef_score * project_score - coef_cost * skill_cost
