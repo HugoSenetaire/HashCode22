4c1982b634815f9c54be76a54a7ce5b81c6817dd
Matthieu Moreau <matthieu.moreau@pasqal.io>
2022-02-24 20:13:18 +0100

    Improved parsing

--- parse_data.py
+++ parse_data.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass
 import pandas as pd
 from paths import PATHS
@@ -6,37 +7,39 @@ from paths import PATHS
 def parse_data(path):
     contributors_data = []
     projects = []
-    # contributors = DataFrame()
+    skills_data = defaultdict(lambda: defaultdict(int))
     with open(path, "r") as f:
-        nb_contributors, nb_projects = f.readline().split(" ")
+        nb_contributors, nb_projects = f.readline().replace("\n", "").split(" ")
         for i in range(int(nb_contributors)):
             contributors_data.append({})
             contributor = contributors_data[-1]
-            contributor["name"], nb_skills = f.readline().split(" ")
+            contributor["name"], nb_skills = f.readline().replace("\n", "").split(" ")
             for j in range(int(nb_skills)):
-                skill_name, skill_level = f.readline().split(" ")
+                skill_name, skill_level = f.readline().replace("\n", "").split(" ")
                 contributor[skill_name] = int(skill_level)
-        contributors = pd.DataFrame(contributors_data).fillna(0)
+                skills_data[skill_name][f"nb_contribs_{int(skill_level)}"] += 1
+                skills_data[skill_name]["nb_contribs"] += 1
 
         for i in range(int(nb_projects)):
-            (
-                project_name,
-                project_length,
-                score,
-                best_before,
-                nb_roles,
-            ) = f.readline().split(" ")
+            (project_name, project_length, score, best_before, nb_roles,) = (
+                f.readline().replace("\n", "").split(" ")
+            )
             roles = []
             for j in range(int(nb_roles)):
-                skill_name, skill_level = f.readline().split(" ")
+                skill_name, skill_level = f.readline().replace("\n", "").split(" ")
                 roles.append(Role(skill_name, skill_level))
+                skills_data[skill_name][f"nb_projects_{int(skill_level)}"] += 1
+                skills_data[skill_name]["nb_projects"] += 1
             projects.append(
                 Project(project_name, project_length, score, best_before, roles)
             )
+    contributors = pd.DataFrame(contributors_data).fillna(0)
+    skills = pd.DataFrame(skills_data).fillna(0)
 
     print("contributors: \n", contributors)
     print("projects:\n", projects)
-    return contributors, projects
+    print("skills:\n", skills)
+    return contributors, projects, skills
 
 
 @dataclass
