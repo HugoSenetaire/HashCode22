25b65f303ec1322c8b0d65d406d7903fe4647fba
HugoSenetaire <hhjs@dtu.dk>
2022-02-24 21:43:35 +0100

    test

--- project_possible_list.py
+++ project_possible_list.py
@@ -9,11 +9,11 @@ def project_possible_list(remaining_projects, persons_list):
                 for skill in person.skills.keys() : 
                     if skill == role : 
                         feasible_role = True 
-                        available_persons.remove(person)
+                        avaible_persons.remove(person)
                         break
-                 if feasible_role : 
+                if feasible_role : 
                      break
-             if !feasible_role : 
-                 available_projects.remove(project);
-                 break
+            if not feasible_role : 
+                available_projects.remove(project)
+                break
     return available_projects
